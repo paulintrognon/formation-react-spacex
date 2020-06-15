02792cafb4ef4bf97b9082c2c86b7ee913d1c1a0
Paulin Trognon <paulin.trognon@gmail.com>
2020-06-15 17:44:44 +0200

    Step 5: Add filter in Launches component

--- src/components/Launches.js
+++ src/components/Launches.js
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Launch from './Launch'
-import { CircularProgress } from '@material-ui/core'
+import { CircularProgress, TextField } from '@material-ui/core'
 
 function Launches({ launches }) {
+  const [filter, setFilter] = useState('');
+
+  const handleChange = (event) => {
+    setFilter(event.target.value);
+  }
+
+  const filteredLaunches = launches && launches.filter(launch => {
+    return launch.mission_name.toLowerCase().indexOf(filter.toLowerCase()) > -1
+  })
+
   return (
     <div className="Launches">
+      <div className="LaunchesSearch">
+        <TextField label="Recherche" onChange={handleChange} />
+      </div>
       <div className="LaunchesList">
         {
-          launches
-            ? launches.map(launch => <Launch key={launch.flight_number} launch={launch}/>)
+          filteredLaunches
+            ? filteredLaunches.map(launch => <Launch key={launch.flight_number} launch={launch}/>)
             : <CircularProgress />
         }
       </div>
