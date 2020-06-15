6cc061040346b24280f7b55f14085aef35d9870a
Paulin Trognon <paulin.trognon@gmail.com>
2020-06-15 17:36:57 +0200

    Step 2: Load launches from SpaceX API

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -1,30 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Axios from 'axios';
 import Launch from '../../components/Launch';
+import { CircularProgress } from '@material-ui/core';
 
 function HomePage() {
-  const launches = [
-    {
-      "flight_number": 1,
-      "mission_name": "FalconSat",
-      "launch_date_utc": "2010-03-24T22:30:00.000Z",
-      "links": {
-        "mission_patch_small": "https://images2.imgbox.com/3c/0e/T8iJcSN3_o.png"
-      }
-    },
-    {
-      "flight_number": 2,
-      "mission_name": "FalconSat 2",
-      "launch_date_utc": "2010-06-01T22:30:00.000Z",
-      "links": {
-        "mission_patch_small": "https://images2.imgbox.com/3c/0e/T8iJcSN3_o.png"
-      }
-    }
-  ];
+  const [launches, setLaunches] = useState();
+
+  useEffect(() => {
+    const url = `https://api.spacexdata.com/v3/launches/past`;
+    Axios.get(url).then((response) => {
+      setLaunches(response.data);    
+    })
+  }, [])
 
   return (
     <div className="Launches">
       <div className="LaunchesList">
-        {launches.map(launch => <Launch key={launch.flight_number} launch={launch} />)}
+        {
+          launches
+            ? launches.map(launch => <Launch key={launch.flight_number} launch={launch} />)
+            : <CircularProgress />
+        }
       </div>
     </div>
   )
